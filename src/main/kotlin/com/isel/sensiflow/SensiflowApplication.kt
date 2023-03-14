0debd15ce3de81af4b3c0e57bf9c241827a23c7c
Francisco Costa <64478921+franciscocostalap@users.noreply.github.com>
2023-03-14 12:44:40 +0000

    Changed workflow so it runs build test and lint at the same time. Added chmod so gradlew has permission to run. (#1)
    * Added exclude dataset since we're not using it yet, so it passes the tests. Changed workflow so it runs build test and linter in the correct order.
    
    * Changed check.yaml so it runs on branch main

--- src/main/kotlin/com/isel/sensiflow/SensiflowApplication.kt
+++ src/main/kotlin/com/isel/sensiflow/SensiflowApplication.kt
@@ -1,9 +1,10 @@
 package com.isel.sensiflow
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class SensiflowApplication
 
 fun main(args: Array<String>) {
