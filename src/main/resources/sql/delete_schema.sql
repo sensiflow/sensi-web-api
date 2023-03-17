8bceb3d36dd5f686f56b4072f920e7576cdf0c21
TiagoJFil <tjgfil@gmail.com>
2023-03-17 19:42:27 +0000

    Created schema for DB

---
+++ src/main/resources/sql/delete_schema.sql
@@ -0,0 +1,6 @@
+delete from "User";
+delete from Token;
+delete from DeviceGroup;
+delete from Device;
+delete from Metric;
+delete from ProcessedStream;
---
+++ src/main/resources/sql/drop_schema.sql
@@ -0,0 +1,6 @@
+drop table processedstream cascade;
+drop table metric cascade;
+drop table devicegroup cascade;
+drop table device cascade;
+drop table token cascade;
+drop table "User" cascade;
