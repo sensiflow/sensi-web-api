000172c2f0cdeaf534cabaf36a7f0c99279901b7
TiagoJFil <tjgfil@gmail.com>
2023-04-29 17:29:16 +0100

    Added an init function to add the default admin user to the db if it does not exist

---
+++ src/main/kotlin/com/isel/sensiflow/DbInit.kt
@@ -0,0 +1,32 @@
+package com.isel.sensiflow
+
+import com.isel.sensiflow.http.entities.input.UserRegisterInput
+import com.isel.sensiflow.model.repository.EmailRepository
+import com.isel.sensiflow.services.Role
+import com.isel.sensiflow.services.UserService
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Component
+
+@Component
+class DbInit(
+    private val userService: UserService,
+    private val emailRepository: EmailRepository,
+) {
+
+    /**
+     * Creates a new admin user in the database if it doesn't exist yet
+     */
+    @PostConstruct
+    private fun init() {
+        if (emailRepository.findByEmail("admin@gmail.com") != null) return
+        userService.createUser(
+            UserRegisterInput(
+                email = "admin@gmail.com",
+                password = "Admin123.",
+                firstName = "Admin",
+                lastName = "Admin"
+            ),
+            Role.ADMIN
+        )
+    }
+}
